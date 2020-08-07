d4fbe47e0d213ca31f58a8f3cacba775bf53cba1
João <jgckruger@gmail.com>
2020-08-07 16:58:42 -0300

    Foundation class

---
+++ classes/Foundation.py
@@ -0,0 +1,50 @@
+from card import Card
+
+suit_names = ['♣', '♦', '♥', '♠']
+colors = ['black', 'red', 'red', 'black']
+
+class Foundation:
+    def __init__(self, suit):
+        self.cards = []   # STACK
+        self.suit = suit
+        self.suit_str = suit_names[suit]
+        self.color = colors[suit]
+
+    def validate_card_into_foundation(self, card: Card):
+        # Checks if the suit is the same
+        if card.suit != self.suit:
+            return False
+        
+        # Check if the card value is the same as the 
+        if len(self.cards) != card.value:
+            return False
+        return True
+
+    def insert_card(self, card):
+        # If it fails, return False
+        if not(self.validate_card_into_foundation(card)):
+            return False
+        # Inserts card into deck
+        self.cards.append(card)
+        return True
+
+    def top(self):
+        if len(self.cards) == 0:
+            return None
+        # Returns last card
+        return self.cards[-1]
+
+    def is_full(self):
+        if len(self.cards) == 13:
+            return True
+        return False
+
+    def get_foundation_values(self):
+        foundation = {
+            'suit': self.suit,
+            'suit_str': self.suit_str,
+            'top_card': self.top(),
+            'is_full': self.is_full()
+        }
+
+        return foundation
