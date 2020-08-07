c14c173b9b764f014706530e4ce06a2883cd6699
João <jgckruger@gmail.com>
2020-08-07 16:16:22 -0300

    Implementation of card attributes as lists

--- classes/Card.py
+++ classes/Card.py
@@ -1,3 +1,7 @@
+card_names = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
+suit_names = ['♣', '♦', '♥', '♠']
+colors = ['black', 'red', 'red', 'black']
+
 class Card:
     value = None
     str_val = None
@@ -5,12 +9,15 @@ class Card:
     color = None
     face_up = False
 
-    def __init__(self, value, suit, color, face_up = False):
+    def __init__(self, value, suit, face_up = False):
         # TODO: Force value to exist within predetermined range
         self.value = value
         self.suit = suit
-        self.color = color
         self.face_up = face_up
+
+        self.str_val = card_names[value]
+        self.suit_name = suit_names[suit]
+        self.color = colors[suit]        
 
     def turn_card(self):
         self.face_up = not(self.face_up)
