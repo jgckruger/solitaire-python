e63102b1223bbf99008d48f8c4c1c5c582407935
João <jgckruger@gmail.com>
2020-08-07 19:42:34 -0300

    Added new game to main

--- main.py
+++ main.py
@@ -1,21 +1,36 @@
+from random import shuffle
 from classes.Card import Card
+from classes.Deck import Deck
 from classes.Foundation import Foundation
 
-# Test cards
-ace_of_spades = Card(0, 3)
-two_of_spades = Card(1, 3)
 
-# Try to insert two spades card into empty spades, should fail
-spades_foundation = Foundation(3)
-spades_foundation.insert_card(two_of_spades)
-print(spades_foundation.get_foundation_values())
+def new_game():
+    cards = []
+    # generates new Deck
+    for i in range(4):      # cards
+        for j in range(13): # suits
+            cards.append(Card(j, i))
+    
+    deck = Deck(cards)
+    deck.shuffle()
+    deck.print()
 
-# Try to insert ace then two of spaces, should achieve success
-spades_foundation = Foundation(3)
-spades_foundation.insert_card(ace_of_spades)
-spades_foundation.insert_card(two_of_spades)
-print(spades_foundation.get_foundation_values())
+new_game()
+# # Test cards
+# ace_of_spades = Card(0, 3)
+# two_of_spades = Card(1, 3)
 
-# Try to insert spades card into hearts, should fail
-hearts_foundation = Foundation(2)
-print('Two of spades into hearts: ', hearts_foundation.insert_card(ace_of_spades))
+# # Try to insert two spades card into empty spades, should fail
+# spades_foundation = Foundation(3)
+# spades_foundation.insert_card(two_of_spades)
+# print(spades_foundation.get_foundation_values())
+
+# # Try to insert ace then two of spaces, should achieve success
+# spades_foundation = Foundation(3)
+# spades_foundation.insert_card(ace_of_spades)
+# spades_foundation.insert_card(two_of_spades)
+# print(spades_foundation.get_foundation_values())
+
+# # Try to insert spades card into hearts, should fail
+# hearts_foundation = Foundation(2)
+# print('Two of spades into hearts: ', hearts_foundation.insert_card(ace_of_spades))
