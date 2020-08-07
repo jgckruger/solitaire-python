46d189904c86e700ccfa9821424adc8e420cd185
João <jgckruger@gmail.com>
2020-08-07 17:01:17 -0300

    Test main for cards

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
 from classes.Card import Card
+from classes.Foundation import Foundation
+
+
 ace_of_spades = Card(0, 3)
-ace_of_spades.str_val
+two_of_spades = Card(1, 3)
+print(ace_of_spades.get_card_values())
+
+hearts_foundation = Foundation(,)
