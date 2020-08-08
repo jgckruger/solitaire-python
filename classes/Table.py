9967168e610ff14e9c3391ce523eaf707644f3a4
João <jgckruger@gmail.com>
2020-08-08 18:26:45 -0300

    Start of the table class for the string representation of the game

--- classes/Table.py
+++ classes/Table.py
@@ -1,3 +1,21 @@
+from classes.Card import Card
+from classes.Hand import Hand
+from classes.Pile import Pile
+from classes.Deck import Deck
+from classes.Foundation import Foundation
+
 class Table:
-    def __init__(self):
-        pass
+    def __init__(self, deck, hand, piles, foundations):
+        self.deck = deck
+        self.hand = hand
+        self.piles = piles
+        self.foundations = foundations
+
+    def check_for_end(self):
+        for foundation in self.foundations:
+            if not(foundation.is_full()):
+                return False
+        return True 
+
+    def print_table(self):
+        print('Deck: ', len(self.deck))
